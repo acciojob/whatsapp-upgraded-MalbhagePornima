0f5a537633d56d904c1bce82dc9f2e738d1a6006
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2024-08-05 15:43:12 +0530

    Update Message.java

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,4 +6,17 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+
+      public Message(String content) {
+        this.content = content;
+    }
+
+    // Getters and Setters
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
