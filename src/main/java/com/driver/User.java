1e931eb493c937254bb7a039db3ec6283ae06585
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2024-08-05 15:41:14 +0530

    Update User.java

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -3,4 +3,26 @@ package com.driver;
 public class User {
     private String name;
     private String mobile;
+
+       public User(String name, String mobileNumber) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
